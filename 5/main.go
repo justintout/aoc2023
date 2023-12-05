c7f35dccf23381f267eb9aa8a54c3cb9dc22b1c6
Justin Tout <3639630+justintout@users.noreply.github.com>
2023-12-05 02:28:29 -0500

    day 5 part 2

--- 5/main.go
+++ 5/main.go
@@ -4,6 +4,7 @@ import (
 	"cmp"
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"regexp"
 	"slices"
@@ -32,7 +33,7 @@ func (c category) apply(n int) int {
 			i = -1
 			break
 		}
-		if n > c.maps[i][1]+c.maps[i][2] {
+		if n > c.maps[i][1]+c.maps[i][2]-1 {
 			continue
 		}
 		break
@@ -118,12 +119,36 @@ func main() {
 		}
 	}
 
-	var locations []int
-	for _, s := range seeds {
-		locations = append(locations, humidityToLocation.apply(temperatureToHumidity.apply(lightToTemperature.apply(waterToLight.apply(fertilizerToWater.apply(soilToFertilizer.apply(seedToSoil.apply(s))))))))
+	solve := func(n int) int {
+		return humidityToLocation.apply(temperatureToHumidity.apply(lightToTemperature.apply(waterToLight.apply(fertilizerToWater.apply(soilToFertilizer.apply(seedToSoil.apply(n)))))))
 	}
 
+	var locations []int
+	for _, s := range seeds {
+		locations = append(locations, solve(s))
+	}
 	slices.Sort(locations)
+	fmt.Printf("part 1: %d\n", locations[0])
 
-	fmt.Println(locations[0])
+	// fmt.Println(seedToSoil.apply(82))
+	// fmt.Println(soilToFertilizer.apply(seedToSoil.apply(82)))
+	// fmt.Println(fertilizerToWater.apply(soilToFertilizer.apply(seedToSoil.apply(82))))
+	// fmt.Println(waterToLight.apply(fertilizerToWater.apply(soilToFertilizer.apply(seedToSoil.apply(82)))))
+	// fmt.Println(lightToTemperature.apply(waterToLight.apply(fertilizerToWater.apply(soilToFertilizer.apply(seedToSoil.apply(82))))))
+	// fmt.Println(temperatureToHumidity.apply(lightToTemperature.apply(waterToLight.apply(fertilizerToWater.apply(soilToFertilizer.apply(seedToSoil.apply(82)))))))
+	// fmt.Println(humidityToLocation.apply(temperatureToHumidity.apply(lightToTemperature.apply(waterToLight.apply(fertilizerToWater.apply(soilToFertilizer.apply(seedToSoil.apply(82))))))))
+
+	// there's probably some smart way to analyze breakpoints to solve this quickly...
+
+	lowest := math.MaxInt
+	for i := 1; i < len(seeds); i += 2 {
+		for j := seeds[i-1]; j < seeds[i-1]+seeds[i]; j++ {
+			l := solve(j)
+			if l < lowest {
+				lowest = l
+			}
+		}
+	}
+
+	fmt.Printf("part 2: %d\n", lowest)
 }
